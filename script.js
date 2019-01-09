e9f5509cbe9bc29177a9f09e3b2d131188fce05a
sureshkatikala <katikalasureshkumar@gmail.com>
2019-01-09 16:24:19 +0530

    Update Script.js

--- script.js
+++ script.js
@@ -7,8 +7,8 @@
            position: "right", // Position may be 'right' or 'left'
            order: "facebook,whatsapp", // Order of buttons
        };
-      //  var proto = document.location.protocol, host = "whatshelp.io", url = proto + "//static." + host;
-      var url = forwardingAddress;
+      var proto = document.location.protocol, host = "whatshelp.io", url = proto + "//static." + host;
+      //var url = forwardingAddress;
        var s = document.createElement('script'); s.type = 'text/javascript'; s.async = true; s.src = url + '/widget-send-button/js/init.js';
        s.onload = function () { WhWidgetSendButton.init(host, proto, options); };
        var x = document.getElementsByTagName('script')[0]; x.parentNode.insertBefore(s, x);
