664f46752996aba88b8a96647cb9a3c2113609ee
Suresh <suresh@crypsis.net>
2019-01-09 16:02:35 +0530

    init file

---
+++ script.js
@@ -0,0 +1,15 @@
+(function () {
+       var options = {
+           facebook: "1629986620602076", // Facebook page ID
+           whatsapp: "+919490339525", // WhatsApp number
+           call_to_action: "Message us", // Call to action
+           button_color: "#FF6550", // Color of button
+           position: "right", // Position may be 'right' or 'left'
+           order: "facebook,whatsapp", // Order of buttons
+       };
+      //  var proto = document.location.protocol, host = "whatshelp.io", url = proto + "//static." + host;
+      var url = forwardingAddress;
+       var s = document.createElement('script'); s.type = 'text/javascript'; s.async = true; s.src = url + '/widget-send-button/js/init.js';
+       s.onload = function () { WhWidgetSendButton.init(host, proto, options); };
+       var x = document.getElementsByTagName('script')[0]; x.parentNode.insertBefore(s, x);
+   })();
