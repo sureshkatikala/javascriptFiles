8e348712e1410c7e7fd13703141897e43c319619
sureshkatikala <katikalasureshkumar@gmail.com>
2019-01-10 15:47:16 +0530

    Updated url to our javascript file

--- script.js
+++ script.js
@@ -9,7 +9,7 @@
        };
       var proto = document.location.protocol, host = "whatshelp.io", url = proto + "//static." + host;
       //var url = forwardingAddress;
-       var s = document.createElement('script'); s.type = 'text/javascript'; s.async = true; s.src = url + '/widget-send-button/js/init.js';
+       var s = document.createElement('script'); s.type = 'text/javascript'; s.async = true; s.src = 'https://cdn.jsdelivr.net/gh/sureshkatikala/javascriptFiles/init.js';
        s.onload = function () { WhWidgetSendButton.init(host, proto, options); };
        var x = document.getElementsByTagName('script')[0]; x.parentNode.insertBefore(s, x);
    })();
