5b31b756e311bdcfa71d1d41d2fcf7965cb0e164
Suresh <suresh@crypsis.net>
2019-01-11 16:19:37 +0530

    modified script.js

--- script1.js
+++ script1.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 (function () {
        var options = {
            facebook: "1629986620602076", // Facebook page ID
@@ -12,25 +11,5 @@
       //var url = forwardingAddress;
        var s = document.createElement('script'); s.type = 'text/javascript'; s.async = true; s.src = 'https://cdn.jsdelivr.net/gh/sureshkatikala/javascriptFiles/init.js';
        s.onload = function () { WhWidgetSendButton.init(host, proto, options); };
-       var x = document.getElementsByTagName('script')[0];
-       console.log(x);
-       x.parentNode.insertBefore(s, x);
+       var x = document.getElementsByTagName('script')[0]; x.parentNode.insertBefore(s, x);
    })();
-=======
-document.body.onload = addElement;
-
-function addElement () { 
-  // create a new div element 
-  var newDiv = document.createElement("div"); 
-  // and give it some content 
-  var newContent = document.createTextNode("Hi there and greetings!"); 
-  // add the text node to the newly created div
-  newDiv.appendChild(newContent);  
-
-  // add the newly created element and its content into the DOM 
-  var currentDiv = document.getElementById("PageContainer"); 
-//   document.body.insertBefore(newDiv, currentDiv); 
-    document.getElementsByTagName('head')[0].appendChild(newContent);
-
-}
->>>>>>> modified script.js
