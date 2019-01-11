22429c43172cc5e7591ab41955e37f3f9377ef82
Suresh <suresh@crypsis.net>
2019-01-11 16:23:34 +0530

    modified script.js

--- script.js
+++ script.js
@@ -1,22 +1,4 @@
-<<<<<<< HEAD
-(function () {
-       var options = {
-           facebook: "1629986620602076", // Facebook page ID
-           whatsapp: "+919490339525", // WhatsApp number
-           call_to_action: "Message us", // Call to action
-           button_color: "#FF6550", // Color of button
-           position: "right", // Position may be 'right' or 'left'
-           order: "facebook,whatsapp", // Order of buttons
-       };
-      var proto = document.location.protocol, host = "whatshelp.io", url = proto + "//static." + host;
-      //var url = forwardingAddress;
-       var s = document.createElement('script'); s.type = 'text/javascript'; s.async = true; s.src = 'https://cdn.jsdelivr.net/gh/sureshkatikala/javascriptFiles/init.js';
-       s.onload = function () { WhWidgetSendButton.init(host, proto, options); };
-       var x = document.getElementsByTagName('script')[0];
-       console.log(x);
-       x.parentNode.insertBefore(s, x);
-   })();
-=======
+
 document.body.onload = addElement;
 
 function addElement () { 
@@ -33,4 +15,3 @@ function addElement () {
     document.getElementsByTagName('head')[0].appendChild(newContent);
 
 }
->>>>>>> modified script.js
