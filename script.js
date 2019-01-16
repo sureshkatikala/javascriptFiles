e03589304afae56b964463cac6ec0cf19cb20e74
sureshkatikala <katikalasureshkumar@gmail.com>
2019-01-16 16:04:23 +0530

    Created div container to hold the icon

--- script.js
+++ script.js
@@ -4,15 +4,18 @@
 // button.style = "bottom:0;right:0;position:relative;z-index: 9999"
 // document.body.appendChild(button);
 
+var newDiv = document.createElement("div"); 
+var x = document.createElement("IMG");
+x.setAttribute("src", "https://image1.jdomni.in/jdomni_email/whatsapp_popup.png?output-format=webp");
+x.setAttribute("width", "48");
+x.setAttribute("height", "48");
+x.setAttribute("alt", "whatsapp icon");
+newDiv.style = "bottom:0;right:0;position:fixed;z-index: 9999"
 
-  var x = document.createElement("IMG");
-  x.setAttribute("src", "https://image1.jdomni.in/jdomni_email/whatsapp_popup.png?output-format=webp");
-  x.setAttribute("width", "48");
-  x.setAttribute("height", "48");
-  x.setAttribute("alt", "whatsapp icon");
-  x.style = "bottom:0;right:0;position:fixed;z-index: 9999"
 x.onclick = function(){
   window.open("https://wa.me/919490339525", "_blank");
 };
-  document.body.appendChild(x);
+
+newDiv.appendChild(x);  
+document.body.appendChild(newDiv);
 
