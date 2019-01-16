f502692049ff753b2236e671a33718699cd2d1d3
sureshkatikala <katikalasureshkumar@gmail.com>
2019-01-16 14:27:50 +0530

    Removed function

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
 
-document.body.onload = addElement;
-
-function addElement () { 
 var button = document.createElement("Button");
 button.innerHTML = "Title";
 button.style = "top:0;right:0;position:absolute;z-index: 9999"
 document.body.appendChild(button);
-}
+
