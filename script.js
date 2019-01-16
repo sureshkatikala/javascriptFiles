0f060a861142f8a3d469b79b4d59a27a49190ff2
sureshkatikala <katikalasureshkumar@gmail.com>
2019-01-16 15:03:48 +0530

    Created whatsapp icon

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 
-var button = document.createElement("Button");
-button.innerHTML = "Title";
-button.style = "bottom:0;right:0;position:relative;z-index: 9999"
-document.body.appendChild(button);
+// var button = document.createElement("Button");
+// button.innerHTML = "Title";
+// button.style = "bottom:0;right:0;position:relative;z-index: 9999"
+// document.body.appendChild(button);
 
+
+  var x = document.createElement("IMG");
+  x.setAttribute("src", "https://image1.jdomni.in/jdomni_email/whatsapp_popup.png?output-format=webp");
+  x.setAttribute("width", "48");
+  x.setAttribute("height", "48");
+  x.setAttribute("alt", "whatsapp icon");
+  x.style = "bottom:0;right:0;position:absolute;z-index: 9999"
+  document.body.appendChild(x);
