9135c15748a76286d223f1685223720819a21570
sureshkatikala <katikalasureshkumar@gmail.com>
2019-01-16 14:46:24 +0530

    Modify the button to bottom

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
 var button = document.createElement("Button");
 button.innerHTML = "Title";
-button.style = "top:0;right:0;position:absolute;z-index: 9999"
+button.style = "bottom:0;right:0;position:absolute;z-index: 9999"
 document.body.appendChild(button);
 
