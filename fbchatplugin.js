06dbcf0adbe199301982cfa4b1e0d2a868636df5
sureshkatikala <katikalasureshkumar@gmail.com>
2019-01-17 17:10:05 +0530

    Update fbchatplugin.js

--- fbchatplugin.js
+++ fbchatplugin.js
@@ -6,15 +6,15 @@
        version          : 'v2.11'
      });
  };
- var js, fjs = document.getElementsByTagName("script")[0];
- js = document.createElement("script");
+ var fjs = document.getElementsById("chat-widget");
+ var js = document.createElement("script");
  js.setAttribute("id", "facebook-jssdk");
  js.setAttribute("src", "https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js");
  fjs.parentNode.insertBefore(js, fjs);
- (function(d, s, id){
-      var js, fjs = d.getElementsByTagName(s)[0];
-      if (d.getElementById(id)) {return;}
-      js = d.createElement(s); js.id = id;
-      js.src = "https://connect.facebook.net/en_US/sdk.js";
-      fjs.parentNode.insertBefore(js, fjs);
-  }(document, 'script', 'facebook-jssdk'));
+//  (function(d, s, id){
+//       var js, fjs = d.getElementsByTagName(s)[0];
+//       if (d.getElementById(id)) {return;}
+//       js = d.createElement(s); js.id = id;
+//       js.src = "https://connect.facebook.net/en_US/sdk.js";
+//       fjs.parentNode.insertBefore(js, fjs);
+//   }(document, 'script', 'facebook-jssdk'));
