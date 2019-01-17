43999b28c2c06251420d162b9e9a152a07ae5f14
sureshkatikala <katikalasureshkumar@gmail.com>
2019-01-17 16:09:06 +0530

    Create fbchatplugin.js

---
+++ fbchatplugin.js
@@ -0,0 +1,20 @@
+ window.fbAsyncInit = function() {
+     FB.init({
+       appId            : '1102875969894081',
+       autoLogAppEvents : true,
+       xfbml            : true,
+       version          : 'v2.11'
+     });
+ };
+ var js, fjs = document.getElementsByTagName("script")[0];
+ js = document.createElement("script");
+ js.setAttribute("id", "facebook-jssdk");
+ js.setAttribute("src", "https://connect.facebook.net/en_US/sdk.js");
+ fjs.parentNode.insertBefore(js, fjs);
+ (function(d, s, id){
+      var js, fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) {return;}
+      js = d.createElement(s); js.id = id;
+      js.src = "https://connect.facebook.net/en_US/sdk.js";
+      fjs.parentNode.insertBefore(js, fjs);
+  }(document, 'script', 'facebook-jssdk'));
