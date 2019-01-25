f296408e36e596710e34ec0cfac48d50039b6066
sureshkatikala <katikalasureshkumar@gmail.com>
2019-01-25 16:58:16 +0530

    Update fbchatplugin.js

--- fbchatplugin.js
+++ fbchatplugin.js
@@ -5,16 +5,17 @@
        xfbml            : true,
        version          : 'v2.11'
      });
+   var fjs = document.getElementById("chat-widget-container");
+   var js = document.createElement("script");
+   js.setAttribute("id", "facebook-jssdk");
+   js.setAttribute("src", "https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js");
+   fjs.appendChild(js);
+  if(fjs.style.display == "none"){
+    FB.CustomerChat.hide();
+  }
  };
 // FB.CustomerChat.hideDialog();
- var fjs = document.getElementById("chat-widget-container");
- var js = document.createElement("script");
- js.setAttribute("id", "facebook-jssdk");
- js.setAttribute("src", "https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js");
- fjs.appendChild(js);
-if(fjs.style.display == "none"){
-//  FB.CustomerChat.hide();
-}
+
 //  (function(d, s, id){
 //       var js, fjs = d.getElementsByTagName(s)[0];
 //       if (d.getElementById(id)) {return;}
