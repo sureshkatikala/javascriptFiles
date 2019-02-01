96c5e61478064ed629217d988a1a9b86c87b1742
sureshkatikala <katikalasureshkumar@gmail.com>
2019-02-01 15:34:57 +0530

    Update script.js

--- script.js
+++ script.js
@@ -162,12 +162,12 @@ closeButton.onclick =  function(){
 //       js.src = "https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js";
 //       fjs.parentNode.insertBefore(js, fjs);
 //     }(document, 'script', 'facebook-jssdk'));
-   var fjs  = document.getElementsByTagName('script')[0];
-   var js = document.createElement("script");
-//    if (document.getElementById("facebook-jssdk")) {return;}
-   js.setAttribute("id", "facebook-jssdk");
-   js.setAttribute("src", "https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js");
-   fjs.appendChild(js);
+//    var fjs  = document.getElementsByTagName('script')[0];
+//    var js = document.createElement("script");
+// //    if (document.getElementById("facebook-jssdk")) {return;}
+//    js.setAttribute("id", "facebook-jssdk");
+//    js.setAttribute("src", "https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js");
+//    fjs.appendChild(js);
      //showOptionsContainer.appendChild(js);
 //   if(fjs.style.display == "none"){
 //     window.FB.CustomerChat.hide();
