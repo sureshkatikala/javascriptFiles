483fe2a83b575c9263e89ff87ada745d87bfb7b9
sureshkatikala <katikalasureshkumar@gmail.com>
2019-02-01 16:59:44 +0530

    Update script.js

--- script.js
+++ script.js
@@ -80,7 +80,7 @@ function showFbChat(d,s,id){
          var js, fjs = d.getElementsByTagName(s)[0];
         if (d.getElementById(id)) {return;}
         js = d.createElement(s); js.id = id;
-        js.src = "https://connect.facebook.net/en_US/sdk.js";
+        js.src = "https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js";
         fjs.parentNode.insertBefore(js, fjs);
 }
 //         (function(d, s, id){
