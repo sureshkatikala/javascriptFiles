f428a1c7f69f10fc4e186ecfa13922fb1f4581a5
sureshkatikala <katikalasureshkumar@gmail.com>
2019-02-07 15:55:12 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,11 +5,11 @@
        xfbml            : false,
        version          : 'v2.11'
      });
-//   FB.Event.subscribe('customerchat.load', function(){
-//    FB.CustomerChat.hide();
+  FB.Event.subscribe('customerchat.load', function(){
+   FB.CustomerChat.showDialog();
    
    
-//   });
+  });
 
   FB.Event.subscribe('customerchat.dialogHide', function(){
    FB.CustomerChat.hide();
