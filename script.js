078a5a23d9182edc3009a6afea17df37786a0674
sureshkatikala <katikalasureshkumar@gmail.com>
2019-02-27 12:59:20 +0530

    Update script.js

--- script.js
+++ script.js
@@ -50,13 +50,13 @@ let whatsappNumber;
 // function  loadWidget(fbPageId, whatsappNumber) {
  
 
-// (function(d, s, id){
-//         var js, fjs = d.getElementsByTagName(s)[0];
-//         if (d.getElementById(id)) {return;}
-//         js = d.createElement(s); js.id = id;
-//         js.src = "https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js";
-//         fjs.parentNode.insertBefore(js, fjs);
-//         }(document, 'script', 'facebook-jssdk'))
+(function(d, s, id){
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) {return;}
+        js = d.createElement(s); js.id = id;
+        js.src = "https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js";
+        fjs.parentNode.insertBefore(js, fjs);
+        }(document, 'script', 'facebook-jssdk'))
 
 // console.log(FB);
 // $(document).on(
@@ -146,13 +146,7 @@ let myVar = setInterval(myTimer, 1000,fbPageId);
 
 function myTimer(fbPageId) {
 if(fbPageId!== 0) {
-        (function(d, s, id){
-        var js, fjs = d.getElementsByTagName(s)[0];
-        if (d.getElementById(id)) {return;}
-        js = d.createElement(s); js.id = id;
-        js.src = "https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js";
-        fjs.parentNode.insertBefore(js, fjs);
-        }(document, 'script', 'facebook-jssdk'))
+        
 
            facebookWidgetContainer.setAttribute('id', 'facebook-widget-container');
          facebookWidgetContainer.style.display = 'none';
